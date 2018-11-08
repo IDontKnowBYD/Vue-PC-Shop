1da8ecb01e4b7c0e4ed6eebbc37eb59cf992dc9a
zhushaofeng <zhushaofeng19960409@gmail.com>
2018-11-08 17:04:09 +0800

    login finished

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Login from '@/components/Login.vue'
+import Home from '@/components/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -14,6 +15,21 @@ export default new Router({
     {
       path: '/login',
       component: Login
+    },
+    {
+      path: '/home',
+      component: Home
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token')
+  if (to.path === '/login' || token) {
+    next()
+  } else {
+    next('/login')
+  }
+})
+
+export default router
